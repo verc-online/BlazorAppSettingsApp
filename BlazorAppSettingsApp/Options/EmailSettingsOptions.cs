714e4f3308e4097229bb431ead27332efbfad9fc
Artemi Vertsinski <verc.online@gmail.com>
2024-05-06 16:07:41 +0300

    Add project files.

---
+++ BlazorAppSettingsApp/Options/EmailSettingsOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorAppSettingsApp.Options
+{
+    public class EmailSettingsOptions
+    {
+        public bool EnableEmailSystem { get; set; }
+        public int EmailTimeoutInSeconds { get; set; }
+        public List<string> EmailServers { get; set; } = new();
+        public AdminOptions EmailAdmin { get; set; } = new();
+    }
+}
